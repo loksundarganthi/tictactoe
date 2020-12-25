e3603bbab9567aa172ca473eb0b417338c40f444
loksundarganthi <76585291+loksundarganthi@users.noreply.github.com>
2020-12-25 16:03:04 +0530

    Add files via upload

---
+++ src/main/java/com/google/devrel/samples/ttt/spi/Ids.java
@@ -0,0 +1,28 @@
+/* Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devrel.samples.ttt.spi;
+
+/**
+ * Contains the client IDs and scopes for allowed clients consuming the tictactoe API.
+ */
+public class Ids {
+  public static final String WEB_CLIENT_ID = "115188427606276446918";
+  public static final String ANDROID_CLIENT_ID = "115188427606276446918";
+  public static final String IOS_CLIENT_ID = "115188427606276446918";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
